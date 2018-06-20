0c7c2b270398fc491d0967ca6da31a586e25d36f
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-06-19 22:54:09 -0300

    adde

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
 const app = express();
 
 app.use(bodyParser.json());
@@ -27,60 +27,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully!',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  console.log('Second middleware');
-
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Post fetched successfully!',
-        posts: documents
-      })
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById({_id: req.params.id}).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found'});
-    }
-  });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = {
-    title: req.body.title,
-    content: req.body.content
-  };
-
-  Post.updateOne({_id: req.params.id}, post).then((result) => {
-    res.status(200).json({message: 'Update successful!'});
-  }).catch(reason => {
-    console.log('*** FROM SERVER: reason ', reason);
-    res.status(500).json({reason});
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    res.status(200).json({message: "Post deleted!"});
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
