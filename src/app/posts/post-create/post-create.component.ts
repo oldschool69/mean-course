b2e9b14d6a8c263ef3b7a1745b329e279e62ff11
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-07-05 10:17:26 -0300

    post image update

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -46,11 +46,12 @@ export class PostCreateComponent implements OnInit {
             id: postData._id,
             title: postData.title,
             content: postData.content,
-            imagePath: null
+            imagePath: postData.imagePath
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
+            image: this.post.imagePath
           });
         }, (error: HttpErrorResponse) => {
            console.error('***Error received from server: ', error);
@@ -86,7 +87,11 @@ export class PostCreateComponent implements OnInit {
       );
     } else {
       console.log('*** UPDATE POST');
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image);
     }
 
     this.form.reset();
