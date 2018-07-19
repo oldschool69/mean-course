8cde44ed618cdd62480dbef23cea1b87e53cc898
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-07-18 23:45:53 -0300

    Criando autenticacao utilizando json web token

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-mongoose.connect('mongodb+srv://oliveira:cox69leo@cluster0-ravys.mongodb.net/test?retryWrites=true')
+mongoose.connect('mongodb+srv://oliveira:cox69leo@cluster0-ravys.mongodb.net/test')
   .then(() => {
     console.log('Connected to the database');
   }).catch((e) => {
@@ -36,5 +37,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
