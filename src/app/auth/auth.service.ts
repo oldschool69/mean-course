886eae2c0f3199f91f52383731c667138dcaea2b
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-07-23 23:36:57 -0300

    create interceptor to add header for each request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from '../../../node_modules/rxjs';
 
 
 
@@ -9,7 +10,18 @@ import { AuthData } from './auth-data.model';
 })
 export class AuthService {
 
+  private token: string;
+  private authStatusListener = new Subject<boolean>();
+
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -21,9 +33,11 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post('http://localhost:3000/api/user/login', authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
+        this.authStatusListener.next(true);
       });
   }
 
