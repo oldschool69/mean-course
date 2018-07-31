95cfb0be6270b812d7970fb47689961c6f86c238
Flavio Oliveira <flavio.marcondes@gmail.com>
2018-07-31 00:03:13 -0300

    Create error handling and error dialogs

---
+++ src/app/error-interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from '../../node_modules/rxjs/operators';
+import { throwError } from '../../node_modules/rxjs';
+import { Injectable } from '../../node_modules/@angular/core';
+import { MatDialog } from '../../node_modules/@angular/material';
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private dialog: MatDialog) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, {data: {message: errorMessage}});
+        return throwError(error);
+      })
+    );
+  }
+}
